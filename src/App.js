81860146cee80f8c21355fa3a14edc6589d4f29a
assilkld <khlifiassil@gmail.com>
2022-09-01 10:13:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: ""
+      search: "",
     };
   }
 
-  search = val => {
+  search = (val) => {
     this.setState({ search: val });
   };
 
